bf28b970a0a27a4505f6be00b2232e900cb554e8
Eugeny Karpov <michmanharitonov1@gmail.com>
2018-07-07 16:01:26 +0300

    add time travel

--- src/main/kotlin/Board.kt
+++ src/main/kotlin/Board.kt
@@ -1,87 +1,20 @@
+import org.w3c.dom.events.Event
 import react.*
 import react.dom.div
 
-class Board : RComponent<RProps, BoardState>() {
+fun RBuilder.board(squares: Array<Player?>, onClickFunction: (Int) -> (Event) -> Unit) {
 
-    override fun componentWillMount() {
-        setState {
-            squares = arrayOfNulls(9)
-            nextPlayer = Player.X
-        }
-    }
-
-    override fun RBuilder.render() {
-
-        div {
-            div("status") { +getStatus(state) }
-
-            for (i in 0..8 step 3) {
-                div("board-row") {
-                    for (j in i..i + 2) {
-                        renderSquare(j)
-                    }
+    div {
+        for (i in 0..8 step 3) {
+            div("board-row") {
+                for (j in i..i + 2) {
+                    renderSquare(j, squares, onClickFunction)
                 }
             }
         }
     }
-
-    private fun RBuilder.renderSquare(i: Int) {
-        square(state.squares[i]) {
-            handleClick(i)
-        }
-    }
-
-    private fun getStatus(boardState: BoardState): String {
-        with(boardState) {
-            val winner = calculateWinner(squares)
-
-            val status = when (winner) {
-                Player.X, Player.O -> "Winner: $winner"
-                else -> "Next player: $nextPlayer"
-            }
-            return status
-        }
-    }
-
-    private fun handleClick(i: Int) {
-        if (calculateWinner(state.squares) != null) {
-            return
-        }
-        val updatedSquares = state.squares.copyOf()
-        updatedSquares[i] = state.nextPlayer
-        setState {
-            squares = updatedSquares
-            nextPlayer = when (nextPlayer) {
-                Player.X -> Player.O
-                Player.O -> Player.X
-            }
-        }
-    }
-
-    private fun calculateWinner(squares: Array<Player?>): Player? {
-        for ((a, b, c) in lines) {
-            if (squares[a] == squares[b] && squares[a] == squares[c]) {
-                return squares[a]
-            }
-        }
-        return null
-    }
-
-    companion object {
-        private val lines = listOf(
-                Triple(0, 1, 2),
-                Triple(3, 4, 5),
-                Triple(6, 7, 8),
-                Triple(0, 3, 6),
-                Triple(1, 4, 7),
-                Triple(2, 5, 8),
-                Triple(0, 4, 8),
-                Triple(2, 4, 6)
-        )
-    }
 }
 
-class BoardState(var squares: Array<Player?>,
-                 var nextPlayer: Player) : RState
-
-fun RBuilder.board() = child(Board::class) {}
+private fun RBuilder.renderSquare(index: Int, squares: Array<Player?>, onClickFunction: (Int) -> (Event) -> Unit) {
+    square(squares[index], onClickFunction(index))
+}
--- src/main/kotlin/Game.kt
+++ src/main/kotlin/Game.kt
@@ -1,24 +1,117 @@
-import react.RBuilder
-import react.RComponent
-import react.RProps
-import react.RState
+import kotlinx.html.js.onClickFunction
+import org.w3c.dom.events.Event
+import react.*
+import react.dom.button
 import react.dom.div
+import react.dom.li
 import react.dom.ol
 
-class Game : RComponent<RProps, RState>() {
+class Game : RComponent<RProps, GameState>() {
+
+    override fun componentWillMount() {
+        setState {
+            history = mutableListOf(HistoryState(arrayOfNulls(9), Player.X))
+        }
+    }
+
     override fun RBuilder.render() {
 
         div("game") {
             div("game-board") {
-                board()
+                board(state.history.last().squares) {i -> {
+                        handleClick(i)
+                    }
+                }
             }
 
             div("game-info") {
-                div {  }
-                ol {  }
+                div { +getStatus(state) }
+                ol { renderMoves() }
             }
         }
     }
+
+    private fun RBuilder.renderMoves() {
+        state.history.mapIndexed { index, _ ->
+            when (index) {
+                0 -> "Go to game start" to index
+                else -> "Go to move #$index" to index
+            }
+        }.forEach { (message, index) ->
+            li {
+                button {
+                    +message
+                    attrs.onClickFunction = jumpToHistoryState(index)
+                }
+            }
+        }
+    }
+
+    private fun jumpToHistoryState(index: Int): (Event) -> Unit {
+        return {
+            setState {
+                history = history.subList(0, index + 1)
+            }
+        }
+    }
+
+    private fun getStatus(gameState: GameState): String {
+        with(gameState) {
+            val current = history.last()
+
+            val winner = calculateWinner(current.squares)
+
+            val status = when (winner) {
+                Player.X, Player.O -> "Winner: $winner"
+                else -> "Next player: ${current.nextPlayer}"
+            }
+            return status
+        }
+    }
+
+    private fun calculateWinner(squares: Array<Player?>): Player? {
+        for ((a, b, c) in lines) {
+            if (squares[a] == squares[b] && squares[a] == squares[c]) {
+                return squares[a]
+            }
+        }
+        return null
+    }
+
+    private fun handleClick(i: Int) {
+        val current = state.history.last()
+
+        if (calculateWinner(current.squares) != null || current.squares[i] != null) {
+            return
+        }
+        val updatedSquares = current.squares.copyOf()
+        updatedSquares[i] = current.nextPlayer
+        setState {
+            val newState = HistoryState(updatedSquares, when (current.nextPlayer) {
+                Player.X -> Player.O
+                Player.O -> Player.X
+            })
+            history = ArrayList(history + newState)
+        }
+    }
+
+    companion object {
+        private val lines = listOf(
+                Triple(0, 1, 2),
+                Triple(3, 4, 5),
+                Triple(6, 7, 8),
+                Triple(0, 3, 6),
+                Triple(1, 4, 7),
+                Triple(2, 5, 8),
+                Triple(0, 4, 8),
+                Triple(2, 4, 6)
+        )
+    }
 }
+
+class GameState(var history: List<HistoryState>) : RState
+
+class HistoryState(var squares: Array<Player?>,
+                   var nextPlayer: Player)
 
 fun RBuilder.game() = child(Game::class) {}
